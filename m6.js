423917abf2cf02865f882c037f2440362789932c
Deanndra <97985953+Deanndra@users.noreply.github.com>
2023-10-04 10:47:56 -0500

    created arrays

---
+++ m6.js
@@ -0,0 +1,13 @@
+const workSchedule = ["Thursday", "Friday", "Saturday", "Sunday"];
+
+console.log(workSchedule[0]);
+console.log(workSchedule[1]);
+console.log(workSchedule[2]);
+console.log(workSchedule[3]);
+
+const timeSchedule = ["7:00am-3:00pm", "9:00am-5:00pm", "9:00am-4:00pm", "9:00am-4:00pm"];
+
+console.log(timeSchedule[0]);
+console.log(timeSchedule[1]);
+console.log(timeSchedule[2]);
+console.log(timeSchedule[3]);
